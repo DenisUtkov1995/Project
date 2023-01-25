4f702b6cd31936a68d376d35e6bc48b5911e6046
Denis Utkov <SKNWoolf@mail.ru>
2023-01-25 13:34:34 +0500

    added currency converter

--- My projects/Calculator v2/script.js
+++ My projects/Calculator v2/script.js
@@ -80,6 +80,8 @@ function handleNumber (numberString){
     }
 }
 
+
+
 function init(){
     document.querySelector('.calc-buttons').addEventListener('click', function(event){
         buttonClick(event.target.innerText);
