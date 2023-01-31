d2498c99320822645abbec10f20c83848c85187d
Denis Utkov <SKNWoolf@mail.ru>
2023-01-31 14:24:52 +0500

    Потестил кнопки

--- My projects/registration for a race/index.js
+++
@@ -1,4 +0,0 @@
-const nameInputNode = document.querySelector(".js-input-name");
-const ageInputNode = document.querySelector(".js-input-age");
-const dateInputNode = document.querySelector(".js-input-date");
-
