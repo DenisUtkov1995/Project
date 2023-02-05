ab818b39390c31c5ebc752ccad4f545a3db752c9
Denis Utkov <SKNWoolf@mail.ru>
2023-02-05 17:09:22 +0500

    worked on the registration for a race

--- My projects/registration for a race/index.js
+++ My projects/registration for a race/index.js
@@ -1,4 +1,58 @@
 const nameInputNode = document.querySelector(".js-input-name");
 const ageInputNode = document.querySelector(".js-input-age");
 const dateInputNode = document.querySelector(".js-input-date");
+const btnNode = document.querySelector(".btn-js");
+const outputNode = document.querySelector('.output-js');
 
+
+
+btnNode.addEventListener ('click', function() {
+    const ageOutput = Number(ageInputNode.value);
+    const nameOutput = String(nameInputNode.value);
+    const dateOutput = Number(dateInputNode.value);
+    let done = true;
+    if (dateOutput > 15) {
+    done = false;
+    }
+    let html ='';
+    if(done && ageOutput < 18 ){
+        html += `
+            <div>
+                ${nameOutput} ваш номер ${firstNumbers()}
+            </div>    
+        `;
+    } else if (!done && ageOutput < 18){
+        html += `
+            <div>
+                ${nameOutput} ваш номер ${lastNumbers()}
+            </div>    
+        `;
+    } else if (ageOutput >= 18){
+        html += `
+            <div>
+                ${nameOutput} ваш номер ${lastNumbers()}
+            </div>    
+        `;
+    }  else {
+        html += `
+            <div>
+                ${nameOutput} Подойдитн к стойке регитсрации
+            </div>    
+        `;
+    }
+    outputNode.innerHTML = html;
+}); 
+
+/*
+function firstRunners() {
+    let output = Number(dateInputNode.value);
+    let done = true;
+    if(output <= 15) {
+        return done;
+    } else {
+        done = false;
+        return done;
+    }
+}
+firstRunners();
+*/
---
+++ My projects/registration for a race/script.js
@@ -0,0 +1,10 @@
+function firstNumbers() {
+    let num = Math.floor(Math.random()*1000);
+    return num;
+}
+
+function lastNumbers() {
+    let num = Math.floor(Math.random() * 1000) + 1000;
+    return num;
+}
+
